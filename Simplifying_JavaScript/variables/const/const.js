ce87a1f7a7e5f433fbd5547b2b19beebffc88697
mckafeh <43886116+mckafeh@users.noreply.github.com>
2023-02-20 17:47:29 +0200

    Tax rate fuction addded

--- Simplifying_JavaScript/variables/const/const.js
+++ Simplifying_JavaScript/variables/const/const.js
@@ -1,2 +1,13 @@
 const taxrate = 0.1;
-const total = 100 + (100 * taxrate);
+const shipping = 5.00;
+const total = 100 + (100 * taxrate) + shipping;
+conts discountable = cart.filter(item => item.discountAvailable);
+
+// Skip 100 lines of code
+return `Your order is ${total}`;
+
+for (let i =0; i < cart.length; i++){
+    if (cart[i].discountAvailable){
+        discountable.push(cart[i]);
+    }
+}
